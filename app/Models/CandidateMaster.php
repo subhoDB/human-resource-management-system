339d9867f5d2ac7de5635f662181dbaf27bf18f5
Subhodeep Bhattacharjee <hi.subho1994@gmail.com>
2023-01-31 21:53:52 +0530

    Modify model

--- app/Models/CandidateMaster.php
+++ app/Models/CandidateMaster.php
@@ -9,9 +9,9 @@ class CandidateMaster extends Model
 {
     use HasFactory;
     public $timestamps = true;
-
+    
     protected $fillable = [
-        'candidate_name',
+        'name',
         'phone_no',
         'email_id',
         'total_experience',
@@ -22,11 +22,9 @@ class CandidateMaster extends Model
         'expected_salary',
         'notice_period',
         'comment',
-        'cv_path',
         'created_by',
         'updated_by',
         'deleted_by',
-        'candidate_status',
-        'is_deleted'
+        'candidate_status'
     ];
 }
